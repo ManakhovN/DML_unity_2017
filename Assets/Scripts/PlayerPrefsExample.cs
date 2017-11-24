ba371d9f893c4c2ba96b2ebe9724bf837455960a
Надир <nadirmanakhov@gmail.com>
2017-11-24 12:44:03 +0300

    playerPrefs example

---
+++ Assets/Scripts/PlayerPrefsExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefsExample : MonoBehaviour {
+    public Text text;
+    public InputField score;
+    public InputField name;
+    void Start () {
+        text.text = "Score = " + PlayerPrefs.GetFloat("Score", 0f) +
+                        " Name = " + PlayerPrefs.GetString("Name", "Jhon");
+	}
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("Score",Single.Parse(score.text));
+        PlayerPrefs.SetString("Name", name.text);
+        PlayerPrefs.Save();
+    }	
+}
